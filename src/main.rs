bff436a6007abfaf6a3aa36827f8a5976944bcda
Ralf Grubenmann <ralf.grubenmann@sdsc.ethz.ch>
2024-02-29 10:59:49 +0100

    add schema compatibility checks form multiple files

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::{
     path::{Path, PathBuf},
 };
 
-use anyhow::Result;
+use anyhow::{bail, Result};
 use apache_avro::{schema_compatibility::SchemaCompatibility, Schema};
 use clap::{arg, command, Parser, Subcommand};
 
@@ -52,36 +52,43 @@ fn visit_dirs(dir: &Path) -> Result<Vec<PathBuf>> {
     Ok(result)
 }
 
-fn validate_schemas(path: &Path) -> Result<()> {
-    let files = visit_dirs(path)?;
+fn parse_schemas(files: Vec<PathBuf>) -> Result<Vec<Schema>> {
     let schemas: Vec<_> = files
         .iter()
         .filter(|f| f.extension().is_some_and(|e| e == "avsc"))
         .map(|f| String::from_utf8_lossy(&fs::read(f).expect("Unable to read file")).into_owned())
         .collect();
     let schemas: Vec<&str> = schemas.iter().map(String::as_str).collect();
-    let _ = Schema::parse_list(&schemas)?;
+    let parsed = Schema::parse_list(&schemas)?;
+    Ok(parsed)
+}
+
+fn validate_schemas(path: &Path) -> Result<()> {
+    let files = visit_dirs(path)?;
+    let _ = parse_schemas(files)?;
     Ok(())
 }
 
 fn compare_schemas(old: &Path, new: &Path) -> Result<()> {
-    let old_schema = Schema::parse_str(
-        String::from_utf8_lossy(
-            &fs::read(old)
-                .unwrap_or_else(|e| panic!("Unable to read file {}: {}", old.display(), e)),
-        )
-        .as_ref(),
-    )
-    .unwrap_or_else(|e| panic!("Couldn't parse schema {}:{}", old.display(), e));
-    let new_schema = Schema::parse_str(
-        String::from_utf8_lossy(
-            &fs::read(new)
-                .unwrap_or_else(|e| panic!("Unable to read file {}: {}", new.display(), e)),
-        )
-        .as_ref(),
-    )
-    .unwrap_or_else(|e| panic!("Couldn't parse schema {}:{}", new.display(), e));
-    SchemaCompatibility::mutual_read(&new_schema, &old_schema)?;
+    let old_files = visit_dirs(old)?;
+    let old_schemas = parse_schemas(old_files)?;
+    let new_files = visit_dirs(new)?;
+    let new_schemas = parse_schemas(new_files)?;
+    for schema in old_schemas {
+        let new_schema = new_schemas
+            .iter()
+            .filter(|s| {
+                s.name().expect("no name on new schema")
+                    == schema.name().expect("no name on old schema")
+            })
+            .next();
+        match new_schema {
+            Some(new_schema) => SchemaCompatibility::mutual_read(&schema, &new_schema)?,
+            None => {
+                bail!("schema {:?} does not exist anymore", schema.name())
+            }
+        }
+    }
     Ok(())
 }
 
@@ -105,4 +112,89 @@ fn main() -> Result<()> {
         }
     }
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use std::fs::File;
+    use std::io::Write;
+
+    use super::*;
+
+    use tempfile::tempdir;
+    fn create_file(dir: &Path, name: &str, content: &str) {
+        let path = dir.join(name);
+        let mut file = File::create(path).unwrap();
+        writeln!(file, "{}", content).unwrap();
+    }
+
+    #[test]
+    fn test_schema_validation() -> Result<()> {
+        let dir = tempdir()?;
+        create_file(
+            &dir.path(),
+            "test.avsc",
+            r#"{
+               "name":"test",
+               "namespace":"my.namespace",
+               "type":"record",
+               "fields":[
+                   {
+                       "name":  "myField",
+                       "doc": "just a field",
+                       "type":"int"
+                   }
+               ] 
+            }"#,
+        );
+        validate_schemas(&dir.path())?;
+        Ok(())
+    }
+
+    #[test]
+    fn test_schema_compatibility() -> Result<()> {
+        let old_dir = tempdir()?;
+        create_file(
+            &old_dir.path(),
+            "test.avsc",
+            r#"{
+               "name":"test",
+               "namespace":"my.namespace",
+               "type":"record",
+               "fields":[
+                   {
+                       "name":  "myField",
+                       "doc": "just a field",
+                       "type":"int"
+                   }
+               ] 
+            }"#,
+        );
+
+        let new_dir = tempdir()?;
+        create_file(
+            &new_dir.path(),
+            "test.avsc",
+            r#"{
+               "name":"test",
+               "namespace":"my.namespace",
+               "type":"record",
+               "fields":[
+                   {
+                       "name":  "myField",
+                       "doc": "just a field",
+                       "type":"int"
+                   },
+                   {
+                       "name":  "myOtherField",
+                       "doc": "just a field",
+                       "type":"int",
+                       "default":1
+                   }
+               ] 
+            }"#,
+        );
+        compare_schemas(old_dir.path(), new_dir.path())?;
+        Ok(())
+    }
 }
